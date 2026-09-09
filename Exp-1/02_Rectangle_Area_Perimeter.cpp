c8f50fad52d68b356e3d9e55d242aad764e02316
Tanvi Tak <taktanvi@gmail.com>
2026-09-09 22:31:49 +0530

    Add Experiment 1 rectangle program

---
+++ Exp-1/02_Rectangle_Area_Perimeter.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main(){ float l,b; cout<<"Enter length and breadth: "; cin>>l>>b; cout<<"Area = "<<l*b<<endl; cout<<"Perimeter = "<<2*(l+b)<<endl; return 0; }
