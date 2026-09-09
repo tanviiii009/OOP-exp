44a36441ec3160c3e4aed446b4fedf4c01b251dc
Tanvi Tak <taktanvi@gmail.com>
2026-09-09 22:32:05 +0530

    Add Experiment 1 temperature program

---
+++ Exp-1/05_Celsius_To_Fahrenheit.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main(){ float c,f; cout<<"Enter temperature in Celsius: "; cin>>c; f=(c*9.0/5.0)+32; cout<<"Temperature in Fahrenheit = "<<f<<endl; return 0; }
