2128deb1d0c33eea34422f910b20504db51d0f15
Tanvi Tak <taktanvi@gmail.com>
2026-09-09 22:32:47 +0530

    Add Exp 3 program 2 - explicit type casting

---
+++ Exp-3/02_Explicit_Type_Casting.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main(){ float num; cout<<"Enter a float value: "; cin>>num; int result=(int)num; cout<<"Float value = "<<num<<endl; cout<<"Integer value after explicit casting = "<<result<<endl; return 0; }
