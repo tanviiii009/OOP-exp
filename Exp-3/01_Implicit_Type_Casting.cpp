13ff340aaab3d18a5de804e16af6dac0f7308ed8
Tanvi Tak <taktanvi@gmail.com>
2026-09-09 22:32:42 +0530

    Add Exp 3 program 1 - implicit type casting

---
+++ Exp-3/01_Implicit_Type_Casting.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main(){ int num=10; double value=num; cout<<"Integer value = "<<num<<endl; cout<<"After implicit casting to double = "<<value<<endl; double d=15.75; int result=d; cout<<"Double value = "<<d<<endl; cout<<"Implicit conversion to integer = "<<result<<endl; return 0; }
