429bbe10f883f016cfece20867b4db01109805c8
Tanvi Tak <taktanvi@gmail.com>
2026-09-09 22:27:12 +0530

    Add Exp 2 program 4 - dynamic memory for array

---
+++ Exp-2/04_Dynamic_Memory_Array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int *arr = new int[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Array elements: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    delete[] arr;
+    arr = nullptr;
+
+    return 0;
+}
